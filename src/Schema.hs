ab92de1ac513fdce2c331f835bcbb219e7aa7856
mike-kob <37714867+mike-kob@users.noreply.github.com>
2019-12-13 15:32:26 +0200

    Define db schema

--- src/Schema.hs
+++ src/Schema.hs
@@ -7,3 +7,36 @@
 {-# LANGUAGE TypeFamilies               #-}
 
 module Schema where
+
+import           Control.Monad.IO.Class
+import           Control.Monad.Logger
+import           Data.Text
+import           Data.Time
+import           Database.Persist.Postgresql
+import           Database.Persist.Sql
+import           Database.Persist.TH
+
+share
+  [mkPersist sqlSettings, mkMigrate "migrateAll"]
+  [persistLowerCase|
+      User
+        userId Int
+        username Text Maybe
+        created UTCTime default=now()
+
+        Primary userId
+        deriving Show
+      Message
+        messageId Int
+        userId UserId
+        text Text
+        sent UTCTime default=now()
+
+        UniqueMsgUser messageId userId
+        Primary messageId
+
+    |]
+
+
+doMigration :: MonadIO m => SqlPersistT m ()
+doMigration = runMigration migrateAll
