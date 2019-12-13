030c288cc342505151ab0c753d12252b2a678613
mike-kob <37714867+mike-kob@users.noreply.github.com>
2019-12-13 14:36:09 +0200

    Start projects, add messages

---
+++ ChangeLog.md
@@ -0,0 +1,3 @@
+# Changelog for HaskellBot
+
+## Unreleased changes
---
+++ src/Schema.hs
@@ -0,0 +1,9 @@
+{-# LANGUAGE ExistentialQuantification  #-}
+{-# LANGUAGE GADTs                      #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE QuasiQuotes                #-}
+{-# LANGUAGE TemplateHaskell            #-}
+{-# LANGUAGE TypeFamilies               #-}
+
+module Schema where
